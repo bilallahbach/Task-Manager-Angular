8c25792e6672dfc25d04744512b3fb7b52af69a2
bilallahbach <lhbachbilal@gmail.com>
2025-03-06 00:33:27 +0000

    1.4

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,12 +3,13 @@ import { RouterOutlet, RouterLink } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FormsModule } from '@angular/forms';  // Import FormsModule here
 import { CommonModule } from '@angular/common'; // if it's a feature module
+import { FooterComponent } from './footer/footer.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet,CommonModule,NavbarComponent, FormsModule],
+  imports: [RouterOutlet,CommonModule,NavbarComponent, FormsModule, FooterComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
